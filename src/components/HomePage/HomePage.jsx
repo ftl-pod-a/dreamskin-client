e0321aee73baf3caf053e1992ff7f85ef5d0d4cf
Evelyn Zhinin Tacuri <ezhinintacuri@ezhinin-ltm73cm.internal.salesforce.com>
2024-07-15 15:56:31 -0700

    added the stats section for the homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -17,6 +17,30 @@ function HomePage(){
                 </div>
             </div>
             <div className='homepage-numbers'>
+                <div className='numbers-content'>
+                    <div className='numbers-par'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dolor placeat, magni ipsam nesciunt cumque nihil eos fugit nam non soluta odit quibusdam unde doloremque nisi fugiat iste quasi eius!</p>
+                    </div>
+                    <div className='number-box'>
+                        <div className='box'>
+                            <i className='fas fa-comments'>Icon</i>
+                            <h3 className='box-number'>Number</h3>
+                        </div>
+                        <div className='box'>
+                            <i className='fas fa-comments'>Icon</i>
+                            <h3 className='box-number'>Number</h3>
+                        </div>
+                        <div className='box'>
+                            <i className='fas fa-comments'>Icon</i>
+                            <h3 className='box-number'>Number</h3>
+                        </div>
+                        <div className='box'>
+                            <i className='fas fa-comments'>Icon</i>
+                            <h3 className='box-number'>Number</h3>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div className='homepage-quiz'>
             </div>
