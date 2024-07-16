2388b343685f8dd57f1c9db359ff4cd3143ac6fb
Elizabeth Johnson <elizabethjohnson@elizabe-ltmnwup.internal.salesforce.com>
2024-07-16 14:16:40 -0700

    Create routine page
    The routine page displays the recommended routine we have generated for a user.
    It utilizes the NavBar component for navigation to other pages, as well as the ProductsList.

--- src/components/RoutinePage/RoutinePage.jsx
+++ src/components/RoutinePage/RoutinePage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./RoutinePage.css";
+import NavBar from '../NavBar/NavBar';
+import ProductsList from '../ProductsList/ProductsList';
 
 const RoutinePage = () => {
     return (
-        <div className="RoutinePage"></div>
+        <div className="RoutinePage">
+            <NavBar />
+            <h1>Routine</h1>
+            <img className="banner-image" src="src/assets/placeholder.jpg" alt="Banner image" />
+            <ProductsList />
+
+        </div>
     )
 
 }
