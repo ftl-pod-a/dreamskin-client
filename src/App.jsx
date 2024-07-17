b0e01d462a36c0949dbe47fbb0e92f22c635f3e7
Elizabeth Johnson <elizabethjohnson@elizabe-ltmnwup.internal.salesforce.com>
2024-07-16 21:43:54 -0700

    added routes for homepage and quizpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import { useState } from 'react';;
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './components/HomePage/HomePage';
+import QuizPage from './components/QuizPage/QuizPage';
+import NavBar from './components/NavBar/NavBar';
 import './App.css';
 
 const App = () => {
@@ -7,6 +11,14 @@ const App = () => {
   return (
     
     <div className='App'>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/quiz' element={<QuizPage />}></Route>
+        </Routes>
+        
+      </BrowserRouter>
     </div>
   )
 }
