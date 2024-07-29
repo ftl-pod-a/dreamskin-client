bd5c49661b961ea884c98fda29dcb35892245578
Evelyn Zhinin Tacuri <ezhinintacuri@ezhinin-ltm73cm.internal.salesforce.com>
2024-07-29 09:40:51 -0700

    Added responsiveness
    Made the pages mobile friendly, specially the navbar with its hamburger style, footer, and skinhub

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,40 +1,43 @@
-import React, { useState, useEffect } from 'react';
-import '@fontsource-variable/dm-sans'
+import React, { useEffect } from 'react';
+import '@fontsource-variable/dm-sans';
 import './NavBar.css';
 import { Link, useNavigate } from "react-router-dom";
 import { useToken } from '../../context/TokenContext';
 
 const NavBar = () => {
-
-    const {tokenContext, setTokenContext} = useToken();
+    const { tokenContext, setTokenContext } = useToken();
     const navigate = useNavigate();
 
     useEffect(() => {
         const authToken = localStorage.getItem('token');
         setTokenContext(authToken)
-    }, [tokenContext])
+    }, [tokenContext]);
 
     const handleLogout = () => {
-        localStorage.clear()
-        setTokenContext("")
+        localStorage.clear();
+        setTokenContext("");
         navigate("/");
     }
+
     return (
-        <>
         <nav className="navbar">
             <div className="content">
-                <div className='logo-content'>
-                    <img src="src/assets/minimalisticLogoUpdated.png" alt="logo" className="logo"/>
+                <div className="logo-content">
+                    <img src="src/assets/minimalisticLogoUpdated.png" alt="logo" className="logo" />
                 </div>
-                <div className='nav-content'>
-                    <div className='tabs'>
+                <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+                <label htmlFor="menu-toggle" className="menu-icon">
+                    <div className="hamburger"></div>
+                </label>
+                <div className="nav-content">
+                    <div className="tabs">
                         <Link to={'/'}>
                             <div>Home</div>
                         </Link>
-                        { tokenContext && 
+                        {tokenContext &&
                             <Link to={'/routine'}>
                                 <div>Routine</div>
-                            </Link> 
+                            </Link>
                         }
                         <Link to={'/skinhub'}>
                             <div>SkinHUB</div>
@@ -44,32 +47,27 @@ const NavBar = () => {
                         </Link>
                         <Link to={'/article'}>
                             <div>Education</div>
-                        </Link>  
+                        </Link>
                     </div>
-                    <div className='buttons'>
-                        { !tokenContext && 
+                    <div className="buttons">
+                        {!tokenContext &&
                             <div>
                                 <Link to={'/register'}>
                                     <button>Log in</button>
                                 </Link>
                             </div>
-                            
                         }
-                        { tokenContext && 
-                            <div className='logged-in'>
+                        {tokenContext &&
+                            <div className="logged-in">
                                 <img src="src/assets/quizicon.jpg" alt="user logo" className="user-image" />
                                 <button className="logout" onClick={handleLogout}>Log Out</button>
                             </div>
-                            
                         }
                     </div>
                 </div>
             </div>
         </nav>
-        </>
     );
-    
-  };
+};
 
 export default NavBar;
-
